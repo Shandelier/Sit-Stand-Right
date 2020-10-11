899f57f71780e5e88e7838bab9c1ad5afd65058a
Carlos <asmonix@gmail.com>
2020-10-11 19:11:44 +0200

    simple implementation

--- src/main.js
+++ src/main.js
@@ -1,10 +1,18 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+import Vuesax from "vuesax";
+import "vuesax/dist/vuesax.css"; //Vuesax styles
+import "@/styles/reset.css";
+import "@/styles/graph-style.css";
 
 Vue.config.productionTip = false;
 
+Vue.use(Vuesax, {
+  // options here
+});
+
 new Vue({
   router,
-  render: h => h(App)
+  render: (h) => h(App),
 }).$mount("#app");
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,122 @@
+/* eslint-disable prettier/prettier */
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="CURVA">
+    <section id="model">
+      <!-- <h1>CURVA - your healthy posture</h1> -->
+      <!-- <h2>Tilt your head to try it out.</h2> -->
+      <vs-card type="1">
+        <template #title>
+          <h3 style="font-style: italic; margin-bottom: 4px">
+            {{ PostureMessage }}
+          </h3>
+        </template>
+        <template #img>
+          <div id="webcam-wrapper">
+            <div class="loader"></div>
+            <canvas
+              id="webcam-pose-canvas"
+              :class="state ? '' : 'webcam-paused'"
+            ></canvas>
+          </div>
+        </template>
+        <template #text>
+          <b>posture feedback</b>
+        </template>
+        <template #interactions>
+          <vs-tooltip>
+            <vs-button
+              @click="webcamState()"
+              primary
+              icon
+              style="margin-left: 20px;"
+            >
+              <b
+                :class="state ? 'bx bx-pause-circle' : 'bx bx-play-circle'"
+              ></b>
+            </vs-button>
+            <template #tooltip>
+              {{ pauseTooltip }}
+            </template>
+          </vs-tooltip>
+        </template>
+      </vs-card>
+      <div id="graph-wrapper"></div>
+    </section>
   </div>
 </template>
+
+<script>
+import { setupModel, cameraState } from "../scripts/model-runner.js";
+import { setupBarGraph, updateBarGraph } from "../scripts/bar-graph.js";
+export default {
+  data() {
+    return {
+      // train your own model using [TODO_TM2_URL], and replace this URL with your own
+      URL: "https://teachablemachine.withgoogle.com/models/rpUwKWKb4/",
+      state: true,
+      prediction: "",
+    };
+  },
+  mounted() {
+    // setupBarGraph is defined in the js/bar-graph.js file
+    setupBarGraph(this.URL);
+    // setupModel is defined in the js/model-runner.js file
+    // we pass in another function: updateBarGraph (defined in the js/bar-graph.js file)
+    // setupModel will store the updateBarGraph function,
+    // and call it every time it has new prediction data from the model
+    setupModel(this.URL, (data) => {
+      updateBarGraph(data);
+      this.prediction = data.reduce((max, prediction) =>
+        max.probability > prediction.probability ? max : prediction
+      ).className;
+    });
+  },
+  methods: {
+    webcamState() {
+      this.state = !this.state;
+      cameraState();
+    },
+  },
+  computed: {
+    pauseTooltip() {
+      if (this.state) {
+        return "Pause pose feedback";
+      } else {
+        return "Continue pose feedback ";
+      }
+    },
+    PostureMessage() {
+      let msg = "";
+      switch (this.prediction) {
+        case "straight":
+          msg = "good posture 🍺";
+          break;
+        case "slouching-front":
+          msg = "you're a caveman 🍖";
+          break;
+        case "slouch-right":
+          msg = "you're leaning right 👉";
+          break;
+        case "slouch-left":
+          msg = "you're leaning left 👈";
+          break;
+
+        default:
+          msg = "Model Loading 👩";
+          break;
+      }
+      return msg;
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.bx {
+  font-size: 1.2rem;
+}
+
+.webcam-paused {
+  filter: blur(1.2px);
+}
+</style>
