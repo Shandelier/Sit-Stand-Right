3642fd27a744f1f55880ce0d4cd1d5dd608e1eba
Carlos <26363920+Shandelier@users.noreply.github.com>
2021-10-02 15:22:27 +0200

    beta feature button and style refactor

---
+++ src/views/BetaApp.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-row>
+    <v-col align="center" justify="center">
+      <h1>Work in progress</h1>
+      <h2>
+        Message
+        <a class="glow" href="https://twitter.com/KarolCodes" target="_blank"
+          >@KarolCodes</a
+        >
+        on twitter for fastesr updates
+      </h2>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+</style>
